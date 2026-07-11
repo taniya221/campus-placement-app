a5ec3433f0b646e8cd9b4cc3ead1e6298ee201fd
taniya221 <taniya.jaison007@gmail.com>
2026-07-11 11:35:37 +0530

    added feature

--- src/Components/AddReg.jsx
+++ src/Components/AddReg.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
 
-const AddReg = () => {
+const = () => {
   return (
-    <div>AddReg</div>
+    <div>
+<div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+<div className="row">
+    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+<label htmlFor="" className="form-control">Registration Number</label>
+<input type="text" className="form-control" />
+    </div>
+    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+<label htmlFor="" className="form-control">Aadhar Number</label>
+<input type="text" className="form-control" />
+    </div>
+    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+<label htmlFor="" className="form-control">Email Address</label>
+<input type="text" className="form-control" />
+    </div>
+    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+<label htmlFor="" className="form-control">Phone Number</label>
+<input type="text" className="form-control" />
+    </div>
+    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+        
+    </div>
+    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6"></div>
+    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6"></div>
+    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6"></div>
+    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6"></div>
+    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6"></div>
+    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6"></div>
+    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6"></div>
+    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6"></div>
+</div>
+        </div>
+    </div>
+</div>
+    </div>
   )
 }
 
-export default AddReg
+export default
--- src/Components/DeleteReg.jsx
+++ src/Components/DeleteReg.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const DeleteReg = () => {
   return (
-    <div>DeleteReg</div>
+    <div>
+.container>.row>.col.col-12.col-sm-12.col-md-12.col-lg-12.col-xl-12.col-xxl-12
+    </div>
   )
 }
 
